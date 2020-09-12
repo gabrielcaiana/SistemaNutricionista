aa04ab97613919342b79f9ed61f2ec4911a827e6
gabrielcaiana <gabrielcaiana@hotmail.com>
2020-09-12 12:38:35 -0300

    criando função para calcular o IMC

--- js/principal.js
+++ js/principal.js
@@ -34,36 +34,8 @@ for (let paciente of pacientes) {
   }
 }
 
-const botãoEnviar = document.querySelector("#adicionar-paciente").addEventListener("click", function(event) {
-    event.preventDefault()
-
-    var form = document.querySelector("#form-adiciona")
-
-    var nome = form.nome.value
-    var peso = form.peso.value
-    var altura = form.altura.value
-    var gordura = form.gordura.value
-
-    var pacienteTr = document.createElement("tr");
-
-    var nomeTd = document.createElement("td");
-    var pesoTd = document.createElement("td");
-    var alturaTd = document.createElement("td");
-    var gorduraTd = document.createElement("td");
-    var imcTd = document.createElement("td");
-
-    nomeTd.textContent = nome
-    pesoTd.textContent = peso
-    alturaTd.textContent = altura
-    gorduraTd.textContent = gordura
-
-    pacienteTr.appendChild(nomeTd)
-    pacienteTr.appendChild(pesoTd)
-    pacienteTr.appendChild(alturaTd)
-    pacienteTr.appendChild(gorduraTd)
-
-    var tabela = document.querySelector("#tabela-pacientes")
-    tabela.appendChild(pacienteTr)
-
-    console.log(pacienteTr)
-})
+function calcularIMC(peso, altura) {
+  var imc = 0
+  imc = peso / (altura * altura)
+  return imc.toFixed(1)
+}
