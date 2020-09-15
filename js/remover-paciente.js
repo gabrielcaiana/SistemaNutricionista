51c1de4f68553ddb7b882d44cedb156d321d1103
gabrielcaiana <gabrielcaiana@hotmail.com>
2020-09-14 23:27:06 -0300

    delegando eventos ao deletar paciente

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,7 +1,6 @@
 var pacientes = document.querySelectorAll(".paciente")
-
-pacientes.forEach(function(paciente){
- paciente.addEventListener("dblclick", function(){
-  paciente.remove()
- })
+var tabela = document.querySelector("table").addEventListener("dblclick", function(event) {
+ var alvoEvento = event.target
+ var paiDoAlvo = alvoEvento.parentNode
+ paiDoAlvo.remove()
 })
