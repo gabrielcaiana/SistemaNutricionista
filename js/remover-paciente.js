ab41893f6d6cc60bf001489cd711f9c9ea8df1fb
gabrielcaiana <gabrielcaiana@hotmail.com>
2020-09-14 23:39:46 -0300

    animando a remoção do paciente

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,6 +1,6 @@
-var pacientes = document.querySelectorAll(".paciente")
 var tabela = document.querySelector("table").addEventListener("dblclick", function(event) {
- var alvoEvento = event.target
- var paiDoAlvo = alvoEvento.parentNode
- paiDoAlvo.remove()
+ event.target.parentNode.classList.add("fadeOut")
+ setTimeout(function(){
+  event.target.parentNode.remove()
+ }, 500)
 })
